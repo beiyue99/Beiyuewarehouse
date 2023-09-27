de2d54a4b7bacce69e29535ae43266892938d7e9
beiyue99 <137810578+beiyue99@users.noreply.github.com>
2023-09-27 23:54:18 +0800

    w

---
+++ mycode/reactor/epoll-web/build/CMakeFiles/app.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "/mnt/d/fileswjf/MyCode/mycode/webserver/epoll-web/exe/app"
+  "/mnt/d/fileswjf/MyCode/mycode/webserver/epoll-web/exe/app.pdb"
+  "CMakeFiles/app.dir/epollweb.c.o"
+  "CMakeFiles/app.dir/epollweb.c.o.d"
+  "CMakeFiles/app.dir/main.c.o"
+  "CMakeFiles/app.dir/main.c.o.d"
+  "CMakeFiles/app.dir/wrap.c.o"
+  "CMakeFiles/app.dir/wrap.c.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
